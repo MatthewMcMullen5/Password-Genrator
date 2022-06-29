ec59d335da27e0d4203453cd6564d006c6c4ed66
Matthew McMullen <matthewmcmullen5@gmail.com>
2022-06-28 19:15:18 -0500

    Simplify and introduce initiate()
    Simplified about 100 lines of code into a simple to read for loop that asks the same questions from a list and applies the answer to the Guide class object.

--- main.py
+++ main.py
@@ -29,128 +29,91 @@ class Guide():
 
 
 r1 = Guide()
-r1.characters = ""
-r1.has_symbols = False
-r1.has_numbers = False
-r1.has_lower = False
-r1.has_upper = False
-r1.has_similar = False
-r1.has_ambiguous = False
-
 yes_or_no = ["yes", "no"]
 
-# input desired generation.
-while True:
-    r1.display()
-    r1.characters = _input("How many characters? ", int)
-    break
 
-while True:
-    r1.display()
-    r1.has_symbols = input("Do you want symbols (e.g. @#$%)? Yes/No ").lower()
+def reset():
+    r = input("Do you want to start over? Yes/No ").lower()
+    if r == "yes":
+        initiate()
+    if r == "no":
+        quit()
 
-    if r1.has_symbols not in yes_or_no:
-        continue
 
-    elif r1.has_symbols == "yes":
-        r1.has_symbols = True
+def initiate():
+    r1.characters = ""
+    r1.has_symbols = False
+    r1.has_numbers = False
+    r1.has_lower = False
+    r1.has_upper = False
+    r1.has_similar = False
+    r1.has_ambiguous = False
 
-    elif r1.has_symbols == "no":
-        r1.has_symbols = False
+    choices = ["How many characters? (press Q at any time to quit/restart) ",
+               "Do you want symbols (e.g. @#$%)? Yes/No ",
+               "Do you want numbers (e.g. 123456)? Yes/No ",
+               "Do you want lower case letters (e.g. abcdefgh)? Yes/No ",
+               "Do you want upper case letters (e.g. ABCDEFGH)? Yes/No ",
+               "Do you want similar characters (e.g. i, l, 1, L, o, 0, O)? Yes/No ",
+               "Do you want ambiguous characters (e.g. { } [ ] ( ) ' ` ~ , ; : . < >)? Yes/No ",
+               "Are the parameters above correct? Yes/No "]
 
-    break
+    x = 0
 
-while True:
-    r1.display()
-    r1.has_numbers = input("Do you want numbers (e.g. 123456)? Yes/No ").lower()
+#   loop through choices and assign values.
+    for y in choices:
+        r1.display()
+        if x == 0:
+            answer = _input(choices[x], int)
+            r1.characters = answer
 
-    if r1.has_numbers not in yes_or_no:
-        continue
+        else:
+            answer = input(choices[x]).lower()
+            if answer == "yes":
+                if x == 1:
+                    r1.has_symbols = True
+                if x == 2:
+                    r1.has_numbers = True
+                if x == 3:
+                    r1.has_lower = True
+                if x == 4:
+                    r1.has_upper = True
+                if x == 5:
+                    r1.has_similar = True
+                if x == 6:
+                    r1.has_ambiguous = True
+                if x == 7:
+                    continue
+            if answer == "no":
+                if x == 1:
+                    r1.has_symbols = False
+                if x == 2:
+                    r1.has_numbers = False
+                if x == 3:
+                    r1.has_lower = False
+                if x == 4:
+                    r1.has_upper = False
+                if x == 5:
+                    r1.has_similar = False
+                if x == 6:
+                    r1.has_ambiguous = False
+                if x == 7:
+                    reset()
+            if answer == "q":
+                reset()
+            if answer not in yes_or_no:
+                continue
 
-    elif r1.has_numbers == "yes":
-        r1.has_numbers = True
+        x += 1
 
-    elif r1.has_numbers == "no":
-        r1.has_numbers = False
 
-    break
-
-while True:
-    r1.display()
-    r1.has_lower = input("Do you want lower case letters (e.g. abcdefgh)? Yes/No ").lower()
-
-    if r1.has_lower not in yes_or_no:
-        continue
-
-    elif r1.has_lower == "yes":
-        r1.has_lower = True
-
-    elif r1.has_lower == "no":
-        r1.has_lower = False
-
-    break
-
-while True:
-    r1.display()
-    r1.has_upper = input("Do you want upper case letters (e.g. ABCDEFGH)? Yes/No ").lower()
-
-    if r1.has_upper not in yes_or_no:
-        continue
-
-    elif r1.has_upper == "yes":
-        r1.has_upper = True
-
-    elif r1.has_upper == "no":
-        r1.has_upper = False
-
-    break
-
-while True:
-    r1.display()
-    r1.has_similar = input("Do you want similar characters (e.g. i, l, 1, L, o, 0, O)? Yes/No ").lower()
-
-    if r1.has_similar not in yes_or_no:
-        continue
-
-    elif r1.has_similar == "yes":
-        r1.has_similar = True
-
-    elif r1.has_similar == "no":
-        r1.has_similar = False
-
-    break
-
-while True:
-    r1.display()
-    r1.has_ambiguous = input("Do you want ambiguous characters (e.g. { } [ ] ( ) ' ` ~ , ; : . < >)? Yes/No ").lower()
-
-    if r1.has_ambiguous not in yes_or_no:
-        continue
-
-    elif r1.has_ambiguous == "yes":
-        r1.has_ambiguous = True
-
-    elif r1.has_ambiguous == "no":
-        r1.has_ambiguous = False
-
-    break
-
-while True:
-    r1.display()
-    is_correct = input("Are the parameters above correct? Yes/No ").lower()
-
-    if is_correct not in yes_or_no:
-        continue
-
-    elif is_correct == "no":
-        exit()
-
-    elif is_correct == "yes":
-        break
+initiate()
 
 # Use information to generate random password
-password_symbols = ["!", "@", "#", "$", "%", "^", "&", "*", "_", "-", "=", "+", "?"]
-password_numbers = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"]
+password_symbols = ["!", "@", "#", "$", "%", "^",
+                    "&", "*", "_", "-", "=", "+", "?"]
+password_numbers = ["1", "2", "3", "4", "5",
+                    "6", "7", "8", "9", "0"]
 password_lower = ["a", "b", "c", "d", "e", "f",
                   "g", "h", "i", "j", "k", "l",
                   "m", "n", "o", "p", "q", "r",
@@ -161,9 +124,12 @@ password_upper = ["A", "B", "C", "D", "E", "F",
                   "M", "N", "O", "P", "Q", "R",
                   "S", "T", "U", "V", "W", "X",
                   "Y", "Z"]
-password_similar = ["i", "I", "l", "1", "L", "o", "O", "0"]
-password_ambiguous = ["{", "}", "[", "]", "(", ")", "/",
-                      "'", "`", "~", ";", ":", ".", "<", ">"]
+password_similar = ["i", "I", "l", "1",
+                    "L", "o", "O", "0"]
+password_ambiguous = ["{", "}", "[", "]",
+                      "(", ")", "/", "'",
+                      "`", "~", ";", ":",
+                      ".", "<", ">"]
 password_key = []
 
 if r1.has_symbols:
@@ -175,7 +141,6 @@ if r1.has_lower:
 if r1.has_upper:
     password_key += password_upper
 if r1.has_similar is False:
-    # for
     password_key.append(r1.has_similar)
 if r1.has_ambiguous:
     password_key += password_ambiguous
@@ -185,18 +150,22 @@ def generate_password():
     password = ""
     for x in range(0, r1.characters):
         password += random.choice(password_key)
-    print(password, "\n")
+    print(password)
 
-
-generate_password()
 
 while True:
+    generate_password()
     repeat = input("Would you like to generate another with these same parameters? ").lower()
     if repeat not in yes_or_no:
         continue
+
     if repeat == "yes":
         for i in range(int(input("How many passwords do you want to generate? "))):
             generate_password()
         continue
+
     if repeat == "no":
-        break
+        reset()
+
+    else:
+        continue
